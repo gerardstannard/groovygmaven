7b07440b8c4513634ccb31ac895d9c00e2feb393
mukadder <hht179@gmail.com>
2013-08-26 22:03:45 -0400

    first commit

---
+++ groovygmaven/src/main/groovy/GroovyGmaven/com/groovygmaven/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package GroovyGmaven.com.groovygmaven
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ groovygmaven/src/main/groovy/GroovyGmaven/com/groovygmaven/Helper.java
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package GroovyGmaven.com.groovygmaven;
+
+/**
+ * Example Java class.
+ */
+public class Helper
+{
+    public void help(final Example example) {
+        example.show();
+    }
+}
